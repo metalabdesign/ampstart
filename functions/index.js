f15a536ec9854d9e5e2b9983837c4d6282041615
Ben Tranter <ben@bentranter.io>
2017-07-13 15:08:37 -0400

    Setup basic backend query filtering
    Adds basic filtering logic on the backend. Should work for some cases, but will get freaked out (I think) if the cities or types value isn't an array.
    
    Also moves the sample data into a JSON file so it's a bit easier to add and delete stuff from there.

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,165 @@
 const functions = require('firebase-functions');
+const fs = require('fs');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+// travelData is the sample data that we use to demo filtering on the frontend.
+const travelData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+/**
+ * cors is HTTP middleware that sets an AMP-specific cross origin resource
+ * sharing HTTP header on our response.
+ * 
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP resonse object.
+ * @param {Function} next - The next HTTP handler to execute.
+ */
+function cors(req, res, next) {
+  res.header(
+    'amp-access-control-allow-source-origin',
+    `${req.protocol}://${req.hostname}:5000`
+  );
+
+  next();
+}
+
+/**
+ * filter returns the travel data that matches the given query.
+ * 
+ * @param {Array} data - Array of objects containing the travel data to
+ * filter.
+ * @param {Object} query - An HTTP request query object.
+ * @return {Array} - An array of travelData objects.
+ */
+function filter(data, query) {
+  var results = [];
+  var push = true;
+
+  // omit every results that doesn't pass _every_ filter
+  data.forEach((val) => {
+    var push = true;
+
+    // if we fail a filter condition, then don't push
+    if (query.maxPrice > 0) {
+      if (val.price.value > query.maxPrice) {
+        push = false;
+      }
+    }
+
+    // check if the city is included
+    if (query.cities.length > 0) {
+      if (!query.cities.includes(val.location.city)) {
+        push = false;
+      }
+    }
+
+    // check if the type is anywhere to be found
+    var found = false;
+    if (query.type.length > 0) {
+      query.type.forEach((type) => {
+        if (val.types.includes(type)) found = true;
+      });
+    } else {
+      // assume it's found if there's no query.type on the request
+      found = true;
+    }
+    if (!found) {
+      push = false;
+    }
+
+    // handle search maybe?? :thinking-face:
+    // just try it with the name to see if it works
+    // but obv search multiple fields
+    if (query.query !== '') {
+      if (!val.name.search(`/${query.query}/i`))
+        push = false;
+    }
+
+    // if we found something, then push it to the results array
+    if (push) {
+      results.push(val);
+    }
+  });
+
+  return results;
+}
+
+/**
+ * Search handles the search and filtering functionality.
+ * 
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ */
+exports.search = functions.https.onRequest((req, res) => {
+
+  // sample data
+  // req.query.maxPrice = 100;
+  // req.query.cities = ["La Paz"];
+  // req.query.type = ["active", "tours"];
+  // req.query.query = "sail";
+
+  cors(req, res, () => {
+    req.query.cities = req.query.cities || []; // TODO for it to be an array even if its a string
+    req.query.maxPrice = req.query.maxPrice || 0;
+    req.query.type = req.query.type || []; // TODO same deal as cities
+    req.query.query = req.query.query || '';
+
+    // Not sure how to handle these yet...
+    // const departureDate = req.query.departure;
+    // const returnDate = req.query.return;
+
+    // Mock data points representing price distribution.
+    var priceData = [
+      0,
+      0,
+      34.091,
+      40.909,
+      53.409,
+      73.864,
+      65.909,
+      28.409,
+      9.091,
+      31.818,
+      100,
+      94.318,
+      56.818,
+      46.023,
+      37.5,
+      0,
+      0,
+    ];
+
+    var results = filter(travelData, req.query);
+
+    const stats = {
+      cities: results.map((result) => ({
+        name: result.location.city,
+        selected: req.query.cities.includes(result.location.city)
+      })),
+      //allCities: !req.query.cities.length || req.query.cities.includes(results.location.city),
+      price: {
+        graph: {
+          pathData: getSVGGraphPathData(priceData, 800, 100),
+          width: 800,
+          height: 100
+        },
+        average: {
+          min: 30.0,
+          max: 100
+        },
+      },
+      location: 'Mexico',
+      resultCount: results.length,
+    }
+    
+    res.json({data: [{
+      stats: stats,
+      results: results,
+    }]});
+
+  });
 });
+
+
+// Helpers
 
 function getSVGGraphPathData(data, width, height) {
   var max = Math.max.apply(null, data);
@@ -48,248 +203,3 @@ function getSVGGraphPathData(data, width, height) {
 
   return commands.join(' ');
 };
-
-
-exports.search = functions.https.onRequest((req, res) => {
-  const host = 'http://localhost:5000';
-  res.header('amp-access-control-allow-source-origin', host);
-
-  console.log(req.query);
-
-  // Mock data points representing price distribution.
-  var priceData = [
-    0,
-    0,
-    34.091,
-    40.909,
-    53.409,
-    73.864,
-    65.909,
-    28.409,
-    9.091,
-    31.818,
-    100,
-    94.318,
-    56.818,
-    46.023,
-    37.5,
-    0,
-    0,
-  ];
-
-  const selectedCities = req.query.cities || [];
-  const cities = [
-    'La Paz',
-    'Cancún',
-    'Mexico City',
-    'Oaxaca',
-    'Puebla',
-    'Tijuana',
-  ];
-
-  // DANKHACKMODE ACTIVATE: https://github.com/ampproject/amphtml/issues/9536
-  res.json({data: [{
-    stats: {
-      cities: cities.map((name) => ({
-        name,
-        selected: selectedCities.includes(name),
-      })),
-      allCities: !selectedCities.length || cities.every((name) => selectedCities.includes(name)),
-      price: {
-        graph: {
-          pathData: getSVGGraphPathData(priceData, 800, 100),
-          width: 800,
-          height: 100
-        },
-        average: {
-          min: 30.0,
-          max: 100
-        },
-      },
-      location: 'Mexico',
-      resultCount: 9,
-    },
-
-    results: [
-      {
-        id: 'xxx-xxx-xx0',
-        name: 'Sail Around the Eastern Mexican Coast',
-        price: {
-          value: 92.0,
-          currency: 'USD',
-        },
-        location: {
-          city: 'La Paz',
-          lat: 0,
-          lng: 0,
-        },
-        reviews: {
-          averageRaiting: 4.0,
-          count: 34,
-        },
-        flags: [],
-        types: ['active', 'tours', 'water', 'nature'],
-      },
-
-      {
-        id: 'xxx-xxx-xx1',
-        name: 'A Tour of the Beaches of Cancún with a Local',
-        price: {
-          value: 70.0,
-          currency: 'USD',
-        },
-        location: {
-          city: 'Cancún',
-          lat: 0,
-          lng: 0,
-        },
-        reviews: {
-          averageRaiting: 5.0,
-          count: 12,
-        },
-        flags: ['new'],
-        types: ['active', 'tours', 'water', 'nature'],
-      },
-
-      {
-        id: 'xxx-xxx-xx2',
-        name: 'Roads of the City',
-        price: {
-          value: 199.0,
-          currency: 'USD',
-        },
-        location: {
-          city: 'Mexico City',
-          lat: 0,
-          lng: 0,
-        },
-        reviews: {
-          averageRaiting: null,
-          count: 0,
-        },
-        flags: ['new'],
-        types: ['tours', 'nightlife'],
-      },
-
-      {
-        id: 'xxx-xxx-xx3',
-        name: 'Beer Excursion of Mexico City',
-        price: {
-          value: 40.0,
-          currency: 'USD',
-        },
-        location: {
-          city: 'Mexico City',
-          lat: 0,
-          lng: 0,
-        },
-        reviews: {
-          averageRaiting: 3.0,
-          count: 1,
-        },
-        flags: ['new'],
-        types: ['bus', 'tours', 'food', 'drinks'],
-      },
-
-      {
-        id: 'xxx-xxx-xx4',
-        name: 'Surf Day. Board and Wetsuits Included in Price!',
-        price: {
-          value: 100.0,
-          currency: 'USD',
-        },
-        location: {
-          city: 'Oaxaca',
-          lat: 0,
-          lng: 0,
-        },
-        reviews: {
-          averageRaiting: 5.0,
-          count: 241,
-        },
-        flags: [],
-        types: ['active', 'water'],
-      },
-
-      {
-        id: 'xxx-xxx-xx5',
-        name: 'Parque Zoologico',
-        price: {
-          value: 20.0,
-          currency: 'USD',
-        },
-        location: {
-          city: 'Mexico City',
-          lat: 0,
-          lng: 0,
-        },
-        reviews: {
-          averageRaiting: 4.5,
-          count: 104,
-        },
-        flags: [],
-        types: ['tours', 'nature'],
-      },
-
-      {
-        id: 'xxx-xxx-xx6',
-        name: 'The Best Coffee in Mexico. Free Coffee for 2 hours',
-        price: {
-          value: 5.0,
-          currency: 'USD',
-        },
-        location: {
-          city: 'Tijuana',
-          lat: 0,
-          lng: 0,
-        },
-        reviews: {
-          averageRaiting: 5.0,
-          count: 2,
-        },
-        flags: ['new'],
-        types: ['food', 'drinks'],
-      },
-
-      {
-        id: 'xxx-xxx-xx7',
-        name: 'Top Fashion Spots with Instagram Blogger',
-        price: {
-          value: 45.0,
-          currency: 'USD',
-        },
-        location: {
-          city: 'Mexico City',
-          lat: 0,
-          lng: 0,
-        },
-        reviews: {
-          averageRaiting: 4.0,
-          count: 66,
-        },
-        flags: [],
-        types: ['fashion', 'artistic'],
-      },
-
-      {
-        id: 'xxx-xxx-xx8',
-        name: 'Mexican Meat Market',
-        price: {
-          value: 19.0,
-          currency: 'USD',
-        },
-        location: {
-          city: 'Mexico City',
-          lat: 0,
-          lng: 0,
-        },
-        reviews: {
-          averageRaiting: 2.0,
-          count: 11,
-        },
-        flags: [],
-        types: ['food'],
-      },
-    ],
-  }]});
-});
